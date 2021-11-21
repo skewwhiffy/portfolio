2e5ae8da1d81fa263b2f61f1bc7369a391d07bc9
Kenny Hung <khung@instructors.2u.com>
2021-11-21 20:34:55 +0000

    Portfolio

--- src/App.js
+++ src/App.js
@@ -56,10 +56,10 @@ WHEN I click on a navigation title
 THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
 WHEN I load the portfolio the first time
 THEN the About Me title and section are selected by default
-
-TODO:
 WHEN I am presented with the About Me section
 THEN I see a recent photo or avatar of the developer and a short bio about them
+
+TODO:
 WHEN I am presented with the Portfolio section
 THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
 WHEN I am presented with the Contact section
--- src/component/Portfolio/index.js
+++ src/component/Portfolio/index.js
@@ -1,9 +1,64 @@
 import React from 'react'
 
 const Portfolio = () => {
+  const sites = [{
+    name: 'Twitter',
+    url: 'https://twitter.com',
+    github: 'https://github.com/twitter',
+    screenshot: 'twitter.png'
+  }, {
+    name: 'XKCD',
+    url: 'https://xkcd.com',
+    github: 'https://github.com/xkcd',
+    screenshot: 'xkcd.png'
+  }, {
+    name: 'Signal',
+    url: 'https://signal.org',
+    github: 'https://github.com/signalapp',
+    screenshot: 'signal.png'
+  }, {
+    name: 'OnePlus',
+    url: 'https://www.oneplus.com',
+    github: 'https://github.com/OnePlusOSS',
+    screenshot: 'oneplus.png'
+  }, {
+    name: 'QMK Keyboards',
+    url: 'https://qmk.fm',
+    github: 'https://github.com/qmk',
+    screenshot: 'qmk.png'
+  }, {
+    name: 'OLKB Keyboards',
+    url: 'https://olkb.com/',
+    github: 'https://github.com/olkb',
+    screenshot: 'olkb.png'
+  }]
   return (
-    <div>
+    <div className="portfolio">
       <h1>Portfolio</h1>
+      <p>
+        <b>The following information is not real! Screenshots and URLs are for demonstration purposes only!</b>
+      </p>
+      {
+        sites.map(it => {
+          return (
+            <div>
+              <h2>{it.name}</h2>
+              <p>
+                <img
+                  className="screenshot"
+                  src={require(`../../asset/portfolio/${it.screenshot}`).default}
+                  alt={`${it.name} screenshot`}
+                />
+                <ul>
+                  <li>URL: <a href={it.url}>{it.url}</a></li>
+                  <li>Github: <a href={it.github}>{it.github}</a></li>
+                </ul>
+              </p>
+              <br />
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
