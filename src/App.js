ad0c493fcf338ee3332088eb0688e3aed12fe52d
Kenny Hung <khung@instructors.2u.com>
2021-11-21 20:05:33 +0000

    About me

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const choices = [
 const App = () => {
   const [currentChoice, setCurrentChoice] = useState(choices[0])
   return (
-    <div>
+    <div className="container">
       <Header
         choices={choices}
         currentChoice={currentChoice}
@@ -52,12 +52,12 @@ WHEN I view the header
 THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
 WHEN I view the navigation titles
 THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
-
-TODO:
 WHEN I click on a navigation title
 THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
 WHEN I load the portfolio the first time
 THEN the About Me title and section are selected by default
+
+TODO:
 WHEN I am presented with the About Me section
 THEN I see a recent photo or avatar of the developer and a short bio about them
 WHEN I am presented with the Portfolio section
