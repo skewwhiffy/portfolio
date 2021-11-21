dd439441b3b2d67f299307abad92a87a03927b55
Kenny Hung <khung@instructors.2u.com>
2021-11-21 13:38:18 +0000

    Set up the sketetal architecture of the solution

---
+++ src/component/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../Navigation'
+
+const Header = () => {
+  return (
+    <div>
+      <p>In the header</p>
+      <Navigation />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/component/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+      <p>In the navigation</p>
+    </div>
+  )
+}
+
+export default Navigation
