f06170c7bc2cafeceebe07c4968f51439b7c1f1d
Kenny Hung <khung@instructors.2u.com>
2021-11-21 21:37:21 +0000

    CV

--- src/component/Resume/index.js
+++ src/component/Resume/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import cv from '../../asset/cv.pdf'
 
 const Resume = () => {
   return (
     <div>
       <h1>Resume</h1>
+      <p>
+        <b>Not a real CV!</b>
+        <br/>
+        <a href="https://africau.edu/images/default/sample.pdf" target="_blank">Download</a>
+      </p>
     </div>
   )
 }
